0238e88ca9f3633bbf86580c5c6c427b89d7e839
Ankush Roy <ankush3411111@gmail.com>
2026-07-23 15:52:39 +0530

    Feat: Fix bg-color of the circle

--- Scenes/Entities/timer_circle.gd
+++ Scenes/Entities/timer_circle.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var color = Color.AQUA
+@export var bg_color = Color.WHITE
 
 var time_left = 2.0
 var init_roation: float = 0
@@ -12,6 +13,8 @@ func _ready() -> void:
 	init_roation = global_rotation
 	$ColorRect.material = $ColorRect.material.duplicate()
 	$ColorRect.material.set_shader_parameter("color", color)
+	$ColorRect.material.set_shader_parameter("bg_color", bg_color)
+
 	$ColorRect.material.set_shader_parameter("val", 0.0)
 
 func _process(delta: float) -> void:
